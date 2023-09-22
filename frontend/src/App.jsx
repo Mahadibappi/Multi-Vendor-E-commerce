fac6f0f43f4b498b7b3c55d090cf7b3bb7fb10f0
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-09-22 11:32:19 +0600

    component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Component } from "./components/Component";
 
 function App() {
   return (
     <div className="App">
-      <h3 className="bg-slate-800 text-white">Hello tailwind</h3>
+      <h3 className="bg-slate-800 text-white">Hey thi is the starting</h3>
+      <Component />
     </div>
   );
 }
