a1f7c2299c0c2dd0312a9bc2faebed0ddc4cf856
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-09-22 11:23:50 +0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className="bg-slate-800 text-white">Hello tailwind</h3>
+    </div>
+  );
+}
+
+export default App;
