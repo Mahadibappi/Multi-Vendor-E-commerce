01a4b58828b1d414799ff5bd2e01e19952ea50c4
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-09-24 19:28:53 +0600

    header created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import { Component } from "./components/Component";
+import router from "./routes/Router";
 
 function App() {
   return (
-    <div className="App">
-      <h3 className="bg-slate-800 text-white">Hey thi is the starting</h3>
-      <Component />
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ frontend/src/routes/Router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../layout/Main";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+  },
+]);
+
+export default router;
