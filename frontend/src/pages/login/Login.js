7a1796a08735bb3f1c78b437fca015f1dc00653c
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-10-02 22:45:00 +0600

    login functionality added

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,42 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 import { Link } from "react-router-dom";
+import { BsGithub } from "react-icons/bs";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const Login = () => {
-  return (
-    <div className=" min-h-screen flex justify-center items-center flex-col">
-      <h1 className="text-center text-2xl font-semibold text-[#3c6d42]">
-        Login To Our Platform
-      </h1>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState("");
 
-      <div className="">
-        <form action="">
-          <div>
-            <input
-              className="w-full py-2 border rounded-2xl px-8 text-center my-5"
-              type="text"
-              placeholder="Email"
-            />
-          </div>
-          <div>
-            <input
-              className="w-full py-2 border rounded-2xl px-8 text-center my-"
-              type="Password"
-              placeholder="Password"
-            />
-          </div>
-          <div className="flex justify-center my-5">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-8 rounded-full">
+  return (
+    <div className="flex min-h-full items-center justify-center">
+      <div className="relative h-[700px] w-[400px] overflow-hidden rounded-3xl">
+        <div
+          className="h-full w-full bg-[100%] "
+          style={{
+            backgroundImage: `url(
+              https://img.freepik.com/free-photo/abstract-digital-grid-black-background_53876-97647.jpg?w=900&t=st=1695564871~exp=1695565471~hmac=4fbe38826442c0a07cec3412127bd88d230f1cee0186b9038a1a1e4046ee4c85
+            )`,
+          }}
+        ></div>
+
+        <div className="absolute bottom-0 flex h-3/4 w-full flex-col rounded-t-3xl bg-cyan-300 bg-opacity-20 shadow ">
+          <form action="#" className="mt-10 space-y-8 px-10 py-10 text-center">
+            <div className="group relative">
+              <input
+                type="text"
+                id="email"
+                autoComplete="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+                className="peer h-12 w-full rounded-3xl bg-gray-100 px-4 text-sm outline-none "
+              />
+              {/* a */}
+              <label
+                htmlFor="email"
+                className="absolute left-1/3 top-0 flex h-full transform items-center pl-2 text-base transition-all duration-300 group-focus-within:-top-7 group-focus-within:h-1/2 group-focus-within:pl-0 group-focus-within:text-base group-focus-within:text-white peer-valid:-top-7 peer-valid:h-1/2 peer-valid:pl-0 peer-valid:text-base peer-valid:text-white"
+              >
+                Email
+              </label>
+            </div>
+            <div className="group relative">
+              <input
+                type={visible ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="peer h-12 w-full rounded-3xl bg-gray-100 px-4 text-sm outline-none"
+              />
+              <label
+                htmlFor="password"
+                className="absolute left-1/3 top-0 flex h-full transform items-center pl-2 text-base transition-all duration-300 group-focus-within:-top-7 group-focus-within:h-1/2 group-focus-within:pl-0 group-focus-within:text-base group-focus-within:text-white peer-valid:-top-7 peer-valid:h-1/2 peer-valid:pl-0 peer-valid:text-base peer-valid:text-white"
+              >
+                Password
+                {visible ? (
+                  <AiOutlineEye
+                    size={20}
+                    className=" mx-20 "
+                    onClick={() => setVisible(false)}
+                  />
+                ) : (
+                  <AiOutlineEyeInvisible
+                    size={20}
+                    className=" mx-20 "
+                    onClick={() => setVisible(true)}
+                  />
+                )}
+              </label>
+            </div>
+            <button className=" h-10 w-full rounded-3xl bg-blue-900 text-white transition-all duration-300 hover: bg-blue-800">
               Login
             </button>
-          </div>
-        </form>
-        <p className="text-md ">
-          Don't Have An Account ?
-          <Link className="mx-1 text-blue-600" to={"/sign-up"}>
-            Sign Up
+          </form>
+          <p className="gap-2 text-center text-white">
+            Don't have an account?
+            <Link
+              to={"/sign-up"}
+              className="font-semibold text-sky-400 hover:text-blue-800"
+            >
+              Sign up
+            </Link>
+          </p>
+          <Link
+            to={"https://github.com"}
+            className="border-white-500 group m-auto my-5 inline-flex h-12 w-[320px] items-center justify-center space-x-2 rounded-3xl border px-4 py-2 transition-colors duration-300 hover:border-black hover:bg-black focus:outline-none"
+          >
+            <span className="h-2.6 w-5 fill-current text-white">
+              <BsGithub />
+            </span>
+            <span className="text-sm font-medium text-white">Github</span>
           </Link>
-        </p>
+        </div>
       </div>
     </div>
   );
